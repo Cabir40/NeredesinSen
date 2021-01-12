b6448f723288b9b9f237e19ab6e824ab2bc7d112
Cabir ÇELİK <64752006+Cabir40@users.noreply.github.com>
2021-01-12 23:11:42 +0300

    Add files via upload

---
+++ app/src/main/java/com/projem/neredesinsenv4/InfoActivity.java
@@ -0,0 +1,17 @@
+package com.projem.neredesinsenv4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+
+    }
+}
---
+++ app/src/main/java/com/projem/neredesinsenv4/Konum.java
@@ -0,0 +1,16 @@
+package com.projem.neredesinsenv4;
+
+
+public class Konum {
+
+    public String mail;
+    public Double latitude;
+    public Double longitude;
+
+    public Konum(String mail, Double latitude , Double longitude ){
+        this.mail = mail;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+    }
+}
